537039980338a6eaaef6a479768dfb0982ce8a0b
nguyenthanhhieu89 <83802242+nguyenthanhhieu89@users.noreply.github.com>
2022-09-06 23:43:38 +0700

    commit

---
+++ web-ticketFilm/src/main/java/hieunt/webticketfilm/WebTicketFilmApplication.java
@@ -0,0 +1,13 @@
+package hieunt.webticketfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTicketFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTicketFilmApplication.class, args);
+	}
+
+}
---
+++ web-ticketFilm/src/test/java/hieunt/webticketfilm/WebTicketFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package hieunt.webticketfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTicketFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
